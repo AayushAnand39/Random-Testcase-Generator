499f09054bd40b5bc7efeb4ee9a246657f4fe2fc
AayushAnand39 <aayushanand737@gmail.com>
2026-08-01 18:05:13 +0530

    Removed ragService.js as it is not needed currently

--- backend/src/services/ragService.js
+++
@@ -1,7 +0,0 @@
-async function getRagContext(query) {
-  // Replace this with Chroma / FAISS / pgvector / Pinecone later.
-  // For now it is a harmless placeholder.
-  return "";
-}
-
-module.exports = { getRagContext };
